a07713779ac60cfe90686b1dc30b087a9dd2ebcf
Jens Birkbak Aagaard <teamtunfiske@gmail.com>
2022-06-28 14:57:23 +0200

    ids

--- features/TextRain/index.tsx
+++ features/TextRain/index.tsx
@@ -1,5 +1,6 @@
 import * as S from "./TextRain.styled"
 import {ReactNode, useEffect, useState} from "react";
+import {v4} from "uuid"
 
 function randomIntFromInterval(min: number, max: number) {
     return (Math.random() * (max - min + 1) + min)
@@ -16,15 +17,28 @@ interface TextRainProps {
 }
 
 const TextRain = (p: TextRainProps) => {
-    const [texts, setTexts] = useState<ReactNode[]>([])
+    const [texts, setTexts] = useState<TextProps[]>([])
+
+    function removeText(id: string) {
+        console.log(id)
+        //setTexts(texts.filter(t => t.id !== id))
+    }
+
     useEffect(() => {
         const interval = setInterval(() => {
-            setTexts(texts => [...texts, <Text size={randomIntFromInterval(p.size[0], p.size[1])}
-                                               text={p.text[Math.round(Math.random() * p.text.length)]}
-                                               rotation={randomIntFromInterval(p.rotation[0], p.rotation[1])}
-                                               opacity={randomIntFromInterval(p.opacity[0], p.opacity[1])}
-                                               textSpeed={randomIntFromInterval(p.textSpeed[0], p.textSpeed[1])}
-                                               left={Math.random() * 100}/>])
+            setTexts(texts => [...texts, {
+                id: v4(),
+                size: randomIntFromInterval(p.size[0], p.size[1]),
+                text: p.text[Math.round(Math.random() * p.text.length)],
+                rotation: randomIntFromInterval(p.rotation[0], p.rotation[1]),
+                opacity: randomIntFromInterval(p.opacity[0], p.opacity[1]),
+                textSpeed: randomIntFromInterval(p.textSpeed[0], p.textSpeed[1]),
+                left: Math.random() * 100,
+                animationCallback: function () {
+                    removeText(this.id)
+                }
+            }
+            ])
         }, p.spawnInterval)
 
         const timeout = setTimeout(() => {
@@ -38,7 +52,7 @@ const TextRain = (p: TextRainProps) => {
     }, [])
     return (
         <S.Wrapper>
-            {texts}
+            {texts.map((t) => <Text {...t} key={t.id}/>)}
         </S.Wrapper>
     )
 }
@@ -46,16 +60,23 @@ const TextRain = (p: TextRainProps) => {
 export default TextRain
 
 export interface TextProps {
+    id: string,
     size: number,
     text: string
     rotation: number,
     opacity: number
     textSpeed: number
     left: number
+    animationCallback: () => void
 }
 
 const Text = (p: TextProps) => {
-
+    useEffect(() => {
+        const timeout = setTimeout(p.animationCallback, p.textSpeed)
+        return () => {
+            clearTimeout(timeout)
+        }
+    }, [])
     return (
         <S.Rain {...p}>{p.text}</S.Rain>
     )
